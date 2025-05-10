acd20f928bfcce9358bdf19d0289d6916254261c
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2025-05-10 15:29:44 -0300

    Criando frontend DashBoard

---
+++ src/Frontend/EcoWatt/EcoWatt/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace EcoWatt
+{
+    partial class DashBoard_Form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button bnt_Home;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashBoard_Form));
+            System.Windows.Forms.Button btn_Qrt1;
+            System.Windows.Forms.Button btn_Qrt2;
+            System.Windows.Forms.Button bnt_sala;
+            System.Windows.Forms.Button btn_sala;
+            System.Windows.Forms.Button btn_Piscina;
+            System.Windows.Forms.Button button1;
+            System.Windows.Forms.Button button2;
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.MenuContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.MenuTransition = new System.Windows.Forms.Timer(this.components);
+            bnt_Home = new System.Windows.Forms.Button();
+            btn_Qrt1 = new System.Windows.Forms.Button();
+            btn_Qrt2 = new System.Windows.Forms.Button();
+            bnt_sala = new System.Windows.Forms.Button();
+            btn_sala = new System.Windows.Forms.Button();
+            btn_Piscina = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.MenuContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bnt_Home
+            // 
+            bnt_Home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(228)))), ((int)(((byte)(235)))));
+            bnt_Home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            bnt_Home.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            bnt_Home.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            bnt_Home.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            bnt_Home.Image = ((System.Drawing.Image)(resources.GetObject("bnt_Home.Image")));
+            bnt_Home.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            bnt_Home.Location = new System.Drawing.Point(0, 0);
+            bnt_Home.Margin = new System.Windows.Forms.Padding(0);
+            bnt_Home.Name = "bnt_Home";
+            bnt_Home.Size = new System.Drawing.Size(183, 55);
+            bnt_Home.TabIndex = 6;
+            bnt_Home.Text = "HOME";
+            bnt_Home.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            bnt_Home.UseVisualStyleBackColor = false;
+            bnt_Home.Click += new System.EventHandler(this.bnt_Home_Click);
+            // 
+            // btn_Qrt1
+            // 
+            btn_Qrt1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            btn_Qrt1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btn_Qrt1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            btn_Qrt1.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btn_Qrt1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            btn_Qrt1.Image = ((System.Drawing.Image)(resources.GetObject("btn_Qrt1.Image")));
+            btn_Qrt1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btn_Qrt1.Location = new System.Drawing.Point(0, 55);
+            btn_Qrt1.Margin = new System.Windows.Forms.Padding(0);
+            btn_Qrt1.Name = "btn_Qrt1";
+            btn_Qrt1.Size = new System.Drawing.Size(205, 54);
+            btn_Qrt1.TabIndex = 7;
+            btn_Qrt1.Text = "QUARTO 1";
+            btn_Qrt1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            btn_Qrt1.UseVisualStyleBackColor = false;
+            // 
+            // btn_Qrt2
+            // 
+            btn_Qrt2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            btn_Qrt2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            btn_Qrt2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            btn_Qrt2.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btn_Qrt2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            btn_Qrt2.Image = ((System.Drawing.Image)(resources.GetObject("btn_Qrt2.Image")));
+            btn_Qrt2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btn_Qrt2.Location = new System.Drawing.Point(0, 109);
+            btn_Qrt2.Margin = new System.Windows.Forms.Padding(0);
+            btn_Qrt2.Name = "btn_Qrt2";
+            btn_Qrt2.Size = new System.Drawing.Size(200, 55);
+            btn_Qrt2.TabIndex = 8;
+            btn_Qrt2.Text = "QUARTO 2";
+            btn_Qrt2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            btn_Qrt2.UseVisualStyleBackColor = false;
+            // 
+            // bnt_sala
+            // 
+            bnt_sala.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            bnt_sala.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            bnt_sala.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            bnt_sala.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            bnt_sala.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            bnt_sala.Image = ((System.Drawing.Image)(resources.GetObject("bnt_sala.Image")));
+            bnt_sala.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            bnt_sala.Location = new System.Drawing.Point(0, 164);
+            bnt_sala.Margin = new System.Windows.Forms.Padding(0);
+            bnt_sala.Name = "bnt_sala";
+            bnt_sala.Size = new System.Drawing.Size(200, 55);
+            bnt_sala.TabIndex = 9;
+            bnt_sala.Text = "SALA";
+            bnt_sala.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            bnt_sala.UseVisualStyleBackColor = false;
+            // 
+            // btn_sala
+            // 
+            btn_sala.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            btn_sala.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            btn_sala.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            btn_sala.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btn_sala.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            btn_sala.Image = ((System.Drawing.Image)(resources.GetObject("btn_sala.Image")));
+            btn_sala.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btn_sala.Location = new System.Drawing.Point(0, 219);
+            btn_sala.Margin = new System.Windows.Forms.Padding(0);
+            btn_sala.Name = "btn_sala";
+            btn_sala.Size = new System.Drawing.Size(200, 55);
+            btn_sala.TabIndex = 10;
+            btn_sala.Text = "COZINHA";
+            btn_sala.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            btn_sala.UseVisualStyleBackColor = false;
+            // 
+            // btn_Piscina
+            // 
+            btn_Piscina.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            btn_Piscina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            btn_Piscina.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            btn_Piscina.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btn_Piscina.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            btn_Piscina.Image = ((System.Drawing.Image)(resources.GetObject("btn_Piscina.Image")));
+            btn_Piscina.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btn_Piscina.Location = new System.Drawing.Point(0, 274);
+            btn_Piscina.Margin = new System.Windows.Forms.Padding(0);
+            btn_Piscina.Name = "btn_Piscina";
+            btn_Piscina.Size = new System.Drawing.Size(200, 55);
+            btn_Piscina.TabIndex = 11;
+            btn_Piscina.Text = "PISCINA";
+            btn_Piscina.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            btn_Piscina.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(78)))), ((int)(((byte)(115)))));
+            this.panel1.Controls.Add(this.flowLayoutPanel2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.MenuContainer);
+            this.flowLayoutPanel2.Controls.Add(button1);
+            this.flowLayoutPanel2.Controls.Add(button2);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(5, 72);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(188, 373);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // MenuContainer
+            // 
+            this.MenuContainer.Controls.Add(bnt_Home);
+            this.MenuContainer.Controls.Add(btn_Qrt1);
+            this.MenuContainer.Controls.Add(btn_Qrt2);
+            this.MenuContainer.Controls.Add(bnt_sala);
+            this.MenuContainer.Controls.Add(btn_sala);
+            this.MenuContainer.Controls.Add(btn_Piscina);
+            this.MenuContainer.Location = new System.Drawing.Point(0, 0);
+            this.MenuContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.MenuContainer.Name = "MenuContainer";
+            this.MenuContainer.Size = new System.Drawing.Size(183, 53);
+            this.MenuContainer.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(5, 5);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(790, 67);
+            this.panel2.TabIndex = 0;
+            // 
+            // MenuTransition
+            // 
+            this.MenuTransition.Interval = 10;
+            this.MenuTransition.Tick += new System.EventHandler(this.MenuTransition_Tick);
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(228)))), ((int)(((byte)(235)))));
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            button1.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            button1.Location = new System.Drawing.Point(0, 53);
+            button1.Margin = new System.Windows.Forms.Padding(0);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(183, 55);
+            button1.TabIndex = 12;
+            button1.Text = "GASTO";
+            button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(228)))), ((int)(((byte)(235)))));
+            button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            button2.Font = new System.Drawing.Font("Impact", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(128)))), ((int)(((byte)(166)))));
+            button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            button2.Location = new System.Drawing.Point(0, 108);
+            button2.Margin = new System.Windows.Forms.Padding(0);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(183, 55);
+            button2.TabIndex = 7;
+            button2.Text = "RANKING";
+            button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // DashBoard_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DashBoard_Form";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.MenuContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel MenuContainer;
+        private System.Windows.Forms.Timer MenuTransition;
+    }
+}
+
---
+++ src/Frontend/EcoWatt/EcoWatt/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EcoWatt
+{
+    public partial class DashBoard_Form : Form
+    {
+        
+        public DashBoard_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        bool menuExpand = false;
+        private void MenuTransition_Tick(object sender, EventArgs e)
+        {
+            if (!menuExpand)
+            {
+                MenuContainer.Height += 10;
+                  if(MenuContainer.Height >= 330)
+                {
+                    MenuTransition.Stop();
+                    menuExpand = true;
+                }
+                
+            }
+            else
+            {
+                MenuContainer.Height -= 10;
+                if(MenuContainer.Height <= 55)
+                {
+                    MenuTransition.Stop();
+                    menuExpand = false;
+                }
+            }
+
+
+        }
+
+        private void bnt_Home_Click(object sender, EventArgs e)
+        {
+            MenuTransition.Start();
+
+        }
+    }
+}
